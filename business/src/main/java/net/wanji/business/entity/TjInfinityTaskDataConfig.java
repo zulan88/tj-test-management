eb8ec154bbbf7df787c8a30a425a13df73609790
guodejun <guodejun@wanji.net.cn>
2024-03-20 17:38:54 +0800

    pageList 添加 DeviceName
    Change-Id: I4e25bf09cf2e5355efc17d70cce0af184f1106ee

--- business/src/main/java/net/wanji/business/entity/TjInfinityTaskDataConfig.java
+++ business/src/main/java/net/wanji/business/entity/TjInfinityTaskDataConfig.java
@@ -59,5 +59,5 @@ public class TjInfinityTaskDataConfig implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableField(exist = false)
-    private TjDeviceDetail tjDeviceDetail = null;
+    private String deviceName;
 }
