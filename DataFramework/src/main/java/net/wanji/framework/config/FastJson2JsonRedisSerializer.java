f1b04c602ed12a456d6166f6464a228b915b8352
hejiakang <hejiakang@wanji.net.cn>
2024-05-07 15:35:02 +0800

    添加 用户user鉴权标识 去除 id排列
    Change-Id: I65608b8ce688a1199216268d757d911b80ee7d14

--- DataFramework/src/main/java/net/wanji/framework/config/FastJson2JsonRedisSerializer.java
+++ DataFramework/src/main/java/net/wanji/framework/config/FastJson2JsonRedisSerializer.java
@@ -52,6 +52,7 @@ public class FastJson2JsonRedisSerializer<T> implements RedisSerializer<T>
         str = str.replace("\"roles\":Set[\"admin\"],","");
         str = str.replace("\"roles\":Set[\"common\"],","");
         str = str.replace("\"roles\":Set[\"dev\"],","");
+        str = str.replace("\"roles\":Set[\"user\"],","");
         str = str.replace("sysUser","user");
         str = str.replace("\"admin\":true,", "\"@type\":\"net.wanji.common.core.domain.entity.SysUser\",\"admin\":true,");
         str = str.replace("\"admin\":false,", "\"@type\":\"net.wanji.common.core.domain.entity.SysUser\",\"admin\":false,");
